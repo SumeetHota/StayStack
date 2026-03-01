d3cde541826f2a6120894d61670a5080785d772d
Git_cmd <sumeethota3405@gmail.com>
2026-03-01 13:46:04 +0530

    update port

--- app.js
+++ app.js
@@ -118,10 +118,10 @@ app.use((err,req,res,next)=>{
 });
 
  
-app.listen(3000,()=>{
-    console.log("Server is listing to your port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
 });
-
 
 
   
